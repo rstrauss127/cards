5af260cd05f20e5f206afc05a39de0b2cf8e735e
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2023-01-03 22:38:14 +0000

    create Flex component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import './App.css';
+//import './App.css';
+import Flex from './components/flex';
 
 function App() {
   return (
-    <div className="App">
-      
-<main class="page-content">
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Linkedin</h2>
-      <p class="copy">Check out my Linkedin</p>
-      <button class="btn">View Linkedin</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Goodreads</h2>
-      <p class="copy">I'm not a big on social media, but I keep my Goodreads up to date.</p>
-      <button class="btn" href='https://www.goodreads.com/user/show/4181830-becca'>Currently Reading</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Spotify</h2>
-      <p class="copy">Check out my favorite music!</p>
-      <button class="btn">My Favorites Playlist</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Other Links</h2>
-      <p class="copy">This will link to some data vizs I have built</p>
-      <button class="btn">More Stuff!!</button>
-    </div>
-  </div>
-</main>
-    </div>
-  );
+    <Flex />
+  )
+
 }
 
 export default App;
