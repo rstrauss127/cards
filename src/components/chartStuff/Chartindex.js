07abc83aacfd9a9dfa1b5655f97fcde991526905
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2023-01-09 21:02:39 +0000

    create files for barchart, import d3

---
+++ src/components/chartStuff/Chartindex.js
@@ -0,0 +1,16 @@
+
+//import './App.css';
+//import Flex from './components/flex';
+
+function ChartIndex() {
+  return (
+    <>
+    <AxisLeft />
+    <AxisBottom />
+    <Marks />
+    </>
+  )
+
+}
+
+export default ChartIndex;
