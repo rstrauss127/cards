7f0e58e84ddf4eb8a8fa0759bbfdcd513c3acf90
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2023-01-11 21:02:58 +0000

    formatted

--- src/components/chartStuff/barChart/useData.js
+++ src/components/chartStuff/barChart/useData.js
@@ -1,5 +1,6 @@
 import { csv } from 'd3';
 import { useState, useEffect } from 'react';
+
 const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
 
 export const useData = () => {
@@ -8,15 +9,18 @@ export const useData = () => {
 
     //useEffect 
     useEffect(() => {
-        //declare row object to be passed to csv()
+        //declare row object to be passed to csv() function from d3
         const row = (d) => {
-            //convert to number, get 2020 numbers
-            d.Population = +d['2020']*1000;
+            //convert to number, get 2019 numbers
+            d.Population = +d['2019']*1000;
+
             return d;
         }
+
         csv(csvUrl, row).then((data) => {
             setData(data.slice(0,10)); //top 10
         })
     }, []);
+
     return data;
 }
