f43264b1ef5c3d7cbe9c6c8b40c83c557f18a5b8
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2023-01-11 18:14:24 +0000

    wrap chart in div for styling

--- src/components/chartStuff/Chartindex.jsx
+++ src/components/chartStuff/Chartindex.jsx
@@ -47,18 +47,20 @@ const ChartIndex = () => {
   const xScale = scaleLinear().domain([0, max(data, xValue)]).range([0, innerWidth]);
 
   return (
-    <svg width={ width } height={ height }>
-      <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <AxisBottom xScale={xScale} innerHeight={innerHeight} tickFormat={ XAxisTickFormat }/>
-        <AxisLeft yScale={yScale} />
-        
-        <text className="axis-label" x={innerWidth / 2} y={innerHeight + xAxisLabelOffset} textAnchor='middle'>
-          Top 10 Countries By Population - 2019
-        </text>
+    <div className='chartBody'>
+      <svg width={ width } height={ height }>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          <AxisBottom xScale={xScale} innerHeight={innerHeight} tickFormat={ XAxisTickFormat }/>
+          <AxisLeft yScale={yScale} />
+          
+          <text className="axis-label" x={innerWidth / 2} y={innerHeight + xAxisLabelOffset} textAnchor='middle'>
+            Top 10 Countries By Population - 2019
+          </text>
 
-        <Marks data={data} xScale={xScale} yScale={yScale} xValue={xValue} yValue={yValue} toolTipFormat={XAxisTickFormat}/>
-      </g>
-    </svg>
+          <Marks data={data} xScale={xScale} yScale={yScale} xValue={xValue} yValue={yValue} toolTipFormat={XAxisTickFormat}/>
+        </g>
+      </svg>
+    </div>
   )
 }
 
